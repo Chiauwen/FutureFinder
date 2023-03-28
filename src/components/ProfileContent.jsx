8d94ceee89575cf544fd75688346c23a7288ebc6
JEAN\szuje <szujean45@gmail.com>
2023-03-28 21:51:40 +0800

    Edit font size for ProfileContent

--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -22,14 +22,14 @@ const Title = styled.p`
 `;
 
 const Description = styled.p`
-    font-size: ${props => props.theme.fontSizes.smallMedium};
+    font-size: 20px;
     font-weight: ${props => props.theme.fontWeights.medium};
     font-family: ${props => props.theme.fonts};
     color: ${props => props.theme.colors.rackley};
     text-align: "left";
 
   ${props => props.theme.breakpoints.down('lg')} {
-    font-size: ${props => props.theme.fontSizes.medium};
+    font-size: ${props => props.theme.fontSizes.smallMedium};
   }
 `;
 
@@ -38,14 +38,14 @@ const Line = styled.div`
 `;
 
 const StyledLi = styled.li`
-  font-size: ${props => props.theme.fontSizes.smallMedium};
+  font-size: 20px;
   font-weight: ${props => props.theme.fontWeights.medium};
   font-family: ${props => props.theme.fonts};
   color: ${props => props.theme.colors.rackley};
   text-align: "left";
 
   ${props => props.theme.breakpoints.down('lg')} {
-    font-size: ${props => props.theme.fontSizes.medium};
+    font-size: ${props => props.theme.fontSizes.smallMedium};
   }
 `;
 
