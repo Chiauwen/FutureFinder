7354e83e285c9ac1e9440aac5650f366193fc536
Chiauwen <chiauwen28@gmail.com>
2023-04-01 14:29:58 +0800

    update ui

--- src/pages/MockInterviewCover.js
+++ src/pages/MockInterviewCover.js
@@ -1,35 +1,57 @@
 import React from 'react'
-import { Header, PageTitle, Footer} from '../components';
-import Container from 'react-bootstrap/Container';
+import { Header, PageTitle, Footer } from '../components'
+import Container from 'react-bootstrap/Container'
 
 import './MockInterviewCover.css'
-import './pages.css';
+import './pages.css'
 
-import puzzle from '../assets/52782.png';
-import { Link } from "react-router-dom";
+import puzzle from '../assets/52782.png'
+import { Link } from 'react-router-dom'
 
 const MockInterviewCover = () => {
   return (
     <div className="Page">
       <Header />
-      <PageTitle title='Mock Interview'/>
+      <PageTitle title="Mock Interview" />
       <Container>
-      <div className='mockinterview-how'>
-        <p>How it works?</p>
-        <div className='mockinterview-p'>
-          <p>FutureFinder’s mock Interview is an AI based program, which all the marks are given by AI based on your facial expression and your answers.</p>
-          <br />
-          <p>The conversational exercise usually resembles a real interview as closely as possible, for the purpose of providing experience for a candidate.</p>
+        <div className="mockinterview-how">
+          <p>How it works?</p>
+          <div className="mockinterview-p">
+            <p>
+              FutureFinder’s mock Interview is an AI based program, which all
+              the marks are given by AI based on your facial expression and your
+              answers.
+            </p>
+            <br />
+            <p>
+              The conversational exercise usually resembles a real interview as
+              closely as possible, for the purpose of providing experience for a
+              candidate.
+            </p>
+          </div>
         </div>
-      </div>
-      <Link to="/mockPage" style={{textDecoration:"none"}}>
+
+        <div className="select">
+          <p>Type of interview:</p>
+
+          <select>
+            <option>General Interview</option>
+            <option>Civil Engineering</option>
+            <option>Data Communication and Networking</option>
+            <option>Medical Science</option>
+            <option>Teacher</option>
+            <option>Nutritionist</option>
+          </select>
+        </div>
+
+        <Link to="/mockPage" style={{ textDecoration: 'none' }}>
           <button className="startbtn">Start Now</button>
         </Link>
-        <img src={puzzle} alt='puzzle'/>
+        <img src={puzzle} alt="puzzle" />
       </Container>
       <Footer />
     </div>
   )
 }
 
-export default MockInterviewCover;
+export default MockInterviewCover
--- src/pages/ProfileMentor.js
+++ src/pages/ProfileMentor.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import { Header, Footer } from '../components'
 import './ProfileMentor.css'
-import {
-  AiOutlineWallet,
-  AiOutlineBank,
-} from 'react-icons/ai'
+import { AiOutlineWallet, AiOutlineBank } from 'react-icons/ai'
 import { RiSecurePaymentFill } from 'react-icons/ri'
 import profilePic from '../assets/profile2.png'
 import { Link } from 'react-router-dom'
@@ -25,7 +22,14 @@ const Profile = () => {
     }
   }
 
-  const course2 = () => {}
+  const course2 = () => {
+    var x = document.getElementById('course2')
+    if (x.style.display === 'none') {
+      x.style.display = 'block'
+    } else {
+      x.style.display = 'none'
+    }
+  }
 
   const renderContent = () => {
     switch (activeButton) {
@@ -94,8 +98,9 @@ const Profile = () => {
           <div className="profileContent">
             <div className="wallet">
               <h1>Wallet</h1>
-              <p>No Withdrawal Method added...</p>
-              <p>Add a new banking method +</p>
+              <h5>No Withdrawal Method added...</h5>
+              <h6>Add a new banking method +</h6>
+
               <AiOutlineWallet />
               <button>Ewallet</button>
               <AiOutlineBank />
@@ -111,25 +116,61 @@ const Profile = () => {
             <div className="services">
               <h1>Current Offered Services</h1>
 
-              <h4>Professional Chat</h4>
+              <h4>1. Professional Chat</h4>
               <p>
                 <strong>Expert Field: </strong>IT/Software Engineering
               </p>
-              <h4>Group Coaching</h4>
-              <h5>Courses:</h5>
+              <h4>2. Group Coaching</h4>
+              <p>
+                <strong>Courses:</strong>
+              </p>
 
-              <button onClick={course1}>
-                Get yourself ready in real IT industry
-              </button>
-              <div id="course1">
-                <h5>descriptions:</h5>
-                <p></p>
+              <div className="course1">
+                <button onClick={course1}>
+                  Get Yourself Ready in Real IT industry
+                </button>
+                <div id="course1">
+                  <h5>descriptions:</h5>
+                  <p>
+                    As a university student, you're likely wondering how you can
+                    prepare yourself for a career in the fast-paced and
+                    ever-changing IT industry. In this talk, we'll discuss some
+                    of the key skills and knowledge that are in high demand in
+                    the industry, as well as strategies for building your own
+                    skills and experience. We'll also explore some of the
+                    challenges and opportunities that await you in the industry,
+                    and how you can best position yourself to succeed. Whether
+                    you're just starting out in your studies or getting ready to
+                    graduate, this talk will give you valuable insights and
+                    practical advice for making your mark in the exciting world
+                    of IT.
+                  </p>
+                  <h6><strong>Length of Courses:</strong> 2 hours</h6>
+                </div>
               </div>
 
-              <button onClick={course2}>AI Evolution</button>
-              <div id='course2'>
-                <h5>descriptions:</h5>
+              <div className="course2">
+                <button onClick={course2}>AI Evolution</button>
+                <div id="course2">
+                  <h5>descriptions:</h5>
+                  <p>
+                    The AI Revolution is a fascinating and rapidly evolving
+                    field that has the potential to transform nearly every
+                    aspect of our lives. This talk is designed to introduce
+                    university students to the basics of AI, including machine
+                    learning, deep learning, and neural networks. We will
+                    explore how AI is already being used in areas like
+                    healthcare, transportation, and finance, and discuss the
+                    ethical and social implications of this technology. Whether
+                    you are an aspiring data scientist or simply curious about
+                    the future of technology, this talk will provide you with a
+                    solid foundation in the exciting field of AI.
+                  </p>
+                  <h6><strong>Length of Courses:</strong> 2 hours</h6>
+                </div>
               </div>
+
+              <button className='add'>Add New Courses +</button>
             </div>
           </div>
         )
